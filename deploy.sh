989c9a75ffc8b7c1d21fd1a26bfbc520d0bb8e6e
1x000 <huaweilaomke402@gmail.com>
2023-01-23 16:27:46 +0700

    %declation%

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+@echo off
+ 
+title GIT一键提交
+color 3
+echo 当前目录是：%cd%
+echo;
+ 
+echo 开始添加变更：git add .
+git add .
+echo;
+ 
+set /p declation=输入提交的commit信息:
+git commit -m "%declation%"
+echo;
+ 
+echo 本地分支拉取远程主分支：git pull origin master
+git push origin main
+echo;
+ 
+echo 执行完毕！
+echo;
+ 
+pause
