abd64567beffe9ac0317e074487afd0f80ae361f
1x000 <huaweilaomke402@gmail.com>
2023-01-24 16:13:25 +0700

    %declation%

---
+++ uptime-status/deploy.sh
@@ -0,0 +1,16 @@
+echo 当前目录是：%cd%
+echo;
+ 
+echo 开始添加变更：git add .
+git add .
+echo;
+ 
+set /p declation=输入提交的commit信息:
+git commit -m "%declation%"
+echo;
+ 
+echo 本地分支拉取远程主分支：git pull origin master
+git push origin main
+echo;
+ 
+pause
